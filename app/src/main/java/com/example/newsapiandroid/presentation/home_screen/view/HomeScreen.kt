92535dae27bf2da6a311a6f4f3cc27d9de9248f8
vaso92 <vasko-vasilev@hotmail.com>
2022-05-05 13:55:01 +0300

    Added news feed button in the side menu.

--- app/src/main/java/com/example/newsapiandroid/presentation/home_screen/view/HomeScreen.kt
+++ app/src/main/java/com/example/newsapiandroid/presentation/home_screen/view/HomeScreen.kt
@@ -8,11 +8,12 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material.icons.outlined.Bookmark
+import androidx.compose.material.icons.outlined.Newspaper
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
@@ -44,9 +45,7 @@ fun HomeScreen() {
             TopBar(
                 navBackStackEntry = currentBackStackEntryAsState,
                 onMenuPressed = {
-                    if (scaffoldState.drawerState.isClosed) {
-                        scope.launch { scaffoldState.drawerState.open() }
-                    }
+                    scope.launch { scaffoldState.drawerState.open() }
                 },
                 onFilterPressed = {},
                 onSearchPressed = { navController.navigate(SearchNewsDestination.route) },
@@ -60,40 +59,36 @@ fun HomeScreen() {
                     },
                     navigationIcon = {
                         IconButton(onClick = {
-                            if (scaffoldState.drawerState.isOpen) {
-                                scope.launch { scaffoldState.drawerState.close() }
-                            }
+                            scope.launch { scaffoldState.drawerState.close() }
                         }) {
                             Icon(Icons.Filled.Close, "close drawer")
                         }
                     }
                 )
             }
+
             Spacer(modifier = Modifier.size(Dimens.grid_2))
-            Button(
-                onClick = { navController.navigate(SavedArticlesDestination.route) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = Dimens.grid_2)
-            ) {
-                Row(Modifier.fillMaxWidth(1f)) {
-                    Icon(
-                        imageVector = Icons.Outlined.Bookmark,
-                        contentDescription = "favorite"
-                    )
-                    Spacer(modifier = Modifier.width(Dimens.grid_5))
-                    Text(
-                        text = stringResource(id = R.string.news_list_saved_articles_button),
-                        style = Typogr.button
-                    )
-                }
-            }
-            Divider(
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-                    .padding(Dimens.grid_2)
-                    .align(alignment = Alignment.CenterHorizontally)
+
+            SideMenuButton(
+                onClick = {
+                    navController.navigate(SavedArticlesDestination.route)
+                    scope.launch { scaffoldState.drawerState.close() }
+                },
+                icon = Icons.Outlined.Bookmark,
+                contentDescription = "favorite",
+                text = stringResource(id = R.string.news_list_saved_articles_button)
             )
+
+            SideMenuButton(
+                onClick = {
+                    navController.navigate(NewsListDestination.route)
+                    scope.launch { scaffoldState.drawerState.close() }
+                },
+                icon = Icons.Outlined.Newspaper,
+                contentDescription = "news feed",
+                text = stringResource(id = R.string.news_list_news_feed_button)
+            )
+            Spacer(modifier = Modifier.size(Dimens.grid_2))
         }
     ) { contentPadding ->
         Box(Modifier.padding(contentPadding)) {
@@ -144,4 +139,31 @@ private fun TopBar(
         },
         elevation = Dimens.grid_2,
     )
+}
+
+@Composable
+fun SideMenuButton(
+    onClick: () -> Unit,
+    text: String,
+    icon: ImageVector,
+    contentDescription: String
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = Dimens.grid_2)
+    ) {
+        Row(Modifier.fillMaxWidth(1f)) {
+            Icon(
+                imageVector = icon,
+                contentDescription = contentDescription
+            )
+            Spacer(modifier = Modifier.width(Dimens.grid_5))
+            Text(
+                text = text,
+                style = Typogr.button
+            )
+        }
+    }
 }
